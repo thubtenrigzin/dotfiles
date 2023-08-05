3ab13ee4bec72ef72f833ef09df3a5f8d866b42e
Thubtenrigzin <thubtenrigzin@gmail.com>
2023-08-05 15:49:13 +0800

    noice options for markdown

--- nvim/lua/custom/configs/noice.lua
+++ nvim/lua/custom/configs/noice.lua
@@ -6,6 +6,11 @@ end
 
 noice.setup {
   lsp = {
+    override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
+		},
     hover = {
       enabled = false,
     },
